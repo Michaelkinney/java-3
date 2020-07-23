1f579ceef19697bc2c96e6547932b00ca3652106
Michael Kinney <mkinney91@gmail.com>
2020-07-22 20:46:30 -0400

    Final push for module 8 (#1)

--- src/concurrency/Main.java
+++ src/concurrency/Main.java
@@ -1,8 +1,12 @@
 package concurrency;
 
 public class Main {
-   
+
     public static void main(String[] args) {
-         //run all of your threads from this main class.
+        concurrency.Reasoning reason = new concurrency.Reasoning();
+        reason.start();
+        concurrency.TeamTC1 tc3 = new concurrency.TeamTC1("V8");
+        Thread t1 = new Thread(tc3);
+        t1.start();
     }
 }
--- src/concurrency/Reasoning.java
+++ src/concurrency/Reasoning.java
@@ -1,10 +1,13 @@
 package concurrency;
 
-class Reasoning {
-    //set up this class so it can become a valid thread. 
-    void distinguish() {
-        //print to the console the difference between a thread and a process
-        //print out you think will happen if you invoke the run() method of a thread as opposed to the start() method of a thread.
+class Reasoning extends Thread{
+
+    public void distinguish() {
+        System.out.println("A Thread is a single action and a process is executing a program.  There are multiple threads within a process.");
+        System.out.println("Start() begins the process where run() doesn't operate on the new thread but on the current thread.");
+    }
+    public void run(){
+        distinguish();
+        System.out.println("Running Reasoning");
     }
 }
-
